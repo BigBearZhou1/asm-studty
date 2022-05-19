ed47e07b487c3cfb43f1583cd82d195093a5878b
BigBearZhou1 <zhouwenzong@126.com>
2022-05-19 23:30:37 +0800

    ClassReader 学习，关注accept()方法

---
+++ src/main/java/transform/BaseTransform.java
@@ -0,0 +1,33 @@
+package transform;
+
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import utils.FileUtils;
+
+public class BaseTransform {
+    public static void main(String[] args) {
+        String relativePath = "sample/Base.class";
+        String filePath = FileUtils.getFilePath(relativePath);
+        byte[] bytes1 = FileUtils.readBytes(filePath);
+
+        ClassReader cr = new ClassReader(bytes1);
+
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+
+        int api = Opcodes.ASM9;
+
+        ClassVisitor cv = new ClassVisitor(api, cw) {
+        };
+
+        int parsingOptions = ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES;
+
+        cr.accept(cv, parsingOptions);
+
+        byte[] bytes2 = cw.toByteArray();
+
+        FileUtils.writeBytes(filePath, bytes2);
+    }
+}
--- src/main/java/utils/FileUtils.java
+++ src/main/java/utils/FileUtils.java
@@ -3,12 +3,12 @@ package utils;
 import java.io.*;
 
 public class FileUtils {
-    public static String getFilePath(String relativePath){
+    public static String getFilePath(String relativePath) {
         String dir = FileUtils.class.getResource("/").getPath();
-        return dir+relativePath;
+        return dir + relativePath;
     }
 
-    public static void writeBytes(String filepath,byte[] bytes){
+    public static void writeBytes(String filepath, byte[] bytes) {
         File file = new File(filepath);
         File dirFile = file.getParentFile();
         mkdirs(dirFile);
@@ -17,13 +17,33 @@ public class FileUtils {
              BufferedOutputStream buff = new BufferedOutputStream(out)) {
             buff.write(bytes);
             buff.flush();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public static byte[] readBytes(String filePath) {
+        File file = new File(filePath);
 
+        if (!file.exists()) {
+            throw new IllegalArgumentException("File Not Exist: " + filePath);
+        }
+
+        try (FileInputStream fin = new FileInputStream(file);
+             BufferedInputStream in = new BufferedInputStream(fin);
+             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = in.read(buffer)) != -1) {
+                out.write(buffer, 0, len);
+            }
+            return out.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     public static void mkdirs(File dirFile) {
         boolean file_exists = dirFile.exists();
@@ -38,7 +58,9 @@ public class FileUtils {
 
         if (!file_exists) {
             boolean flag = dirFile.mkdirs();
-            System.out.println("mkdirs res = "+flag);
+            System.out.println("mkdirs res = " + flag);
         }
     }
+
+
 }
